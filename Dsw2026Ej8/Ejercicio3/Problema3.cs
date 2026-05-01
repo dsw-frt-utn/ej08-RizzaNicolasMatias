98564e335ed05a280998279c13186c5b44ab34a4
Nicolas Rizza <nicolasrizza756@gmail.com>
2026-05-01 14:47:23 -0300

    Resolucion Problema 3:Comparar Copias

---
+++ Dsw2026Ej8/Ejercicio3/Problema3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EJ08_RizzaNicolasMatias.Problema3
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copiaValor = originalValue;
+
+            copiaValor++;
+
+            Product copiaProducto = product;
+
+            copiaProducto.ModificarDescripcion("Nueva Descripción");
+            return $"{originalValue}-{copiaValor}-{copiaProducto.Description}";
+        }
+    }
+}
