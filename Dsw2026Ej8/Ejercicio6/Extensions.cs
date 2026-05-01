f8e5ec6c9df8e2ec6ddd4c79031147a36f546c03
Nicolas Rizza <nicolasrizza756@gmail.com>
2026-05-01 15:15:13 -0300

    Resolucion Ejercicio 6

---
+++ Dsw2026Ej8/Ejercicio6/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EJ08_RizzaNicolasMatias.Problema6
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            if (code == null)
+            {
+                return "SIN-CODIGO";
+            }
+
+            return code.Trim().ToUpper().Replace(" ", "-");
+        }
+    }
+}
---
+++ Dsw2026Ej8/Ejercicio6/Problema6.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EJ08_RizzaNicolasMatias.Problema6
+{
+    public class Problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            return code.ToProductCode();
+        }
+    }
+}
