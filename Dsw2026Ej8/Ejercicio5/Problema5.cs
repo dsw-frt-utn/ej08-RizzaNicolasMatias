63b82cc11eb445de1ba4055198d3250cac532ebf
Nicolas Rizza <nicolasrizza756@gmail.com>
2026-05-01 15:08:30 -0300

    Resolucion Ejercicio 5

---
+++ Dsw2026Ej8/Ejercicio5/Problema5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EJ08_RizzaNicolasMatias.Problema5
+{   
+    public class Sale
+    {
+        public decimal Amount { get; set; } 
+        public virtual decimal CalculateTotal()
+        {
+            return Amount;
+        }
+    }
+
+    public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return Amount;
+        }
+    }
+
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return Amount * 0.90m;
+        }
+    }
+
+    public class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+}
