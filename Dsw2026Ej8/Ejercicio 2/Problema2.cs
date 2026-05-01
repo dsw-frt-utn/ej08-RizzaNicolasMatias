f2e4e2211050968a4f473615f2e1ac46e78cb820
Nicolas Rizza <nicolasrizza756@gmail.com>
2026-05-01 13:54:05 -0300

    Resolucion problema 2:CrearResumenVenta

---
+++ Dsw2026Ej8/Ejercicio 2/Problema2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EJ08_RizzaNicolasMatias.Problema2
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, decimal unitPrice, int quantity)
+        {
+            decimal total = unitPrice * quantity;
+
+            return $"Resumen de Venta:\n" +
+                   $"Producto: [{productCode}] {productDescription}\n" +
+                   $"Precio Unitario: {unitPrice:C}\n" +
+                   $"Cantidad: {quantity}\n" +
+                   $"Total: {total:C}";
+        }
+    }
+}
